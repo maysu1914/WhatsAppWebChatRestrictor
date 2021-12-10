1c2a33dac6509cda197ae15dffb493682e83a60e
maysu1914 <mustafa.aysu@hotmail.com>
2021-12-10 21:05:01 +0300

    new structure, same logic

--- WhatsAppWebChatRestrictor.js
+++ WhatsAppWebChatRestrictor.js
@@ -1,211 +1,211 @@
-init();
-
-function init() {
-    console.log('System initializing...');
-    self.isPageReady = false;
-    self.final_chats = [
-        'Chat 1',
-        'Chat 2',
-        'Group 1'
-    ]
-    chat_class = "_3m_Xw"; //    /html/body/div/div[1]/div[1]/div[3]/div/div/div[1]/div/div/div[1]
-    chat_name_class = "ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr i0jNr"; //    /html/body/div/div[1]/div[1]/div[3]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div[1]/div[1]/span
-    chat_pane_id = 'pane-side';
-    real_click_class = "_2nY6U"; //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div
-    chat_mini_image_class = "_3GlyB"; //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div/div
-    chat_height_dominant_class = "_2nY6U"; //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div
-    multiple_pop_class = '_3J6wB'; //    /html/body/div/div[1]/div/div/div/div
-    multiple_pop_button_1_class = "_20C5O _1zOyO" //    /html/body/div/div[1]/div/div/div/div/div/div[2]/div[1]
-    multiple_pop_button_2_class = "_20C5O _2Zdgs" //    /html/body/div[1]/div/div/div/div/div/div/div[2]/div[2]
-    // scroll bar class for paneside, should be removed
-    pane_side_remove_class = '_20c87' //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]
-    /* header, chat search bar, */
-    will_be_removed = ['header', 'div.uwk68'] //    /html/body/div/div[1]/div[1]/div[3]/div/header, /html/body/div/div[1]/div[1]/div[3]/div/div[1]
-    addCSS();
-
-    chatObserver();
-}
-
-function addCSS() {
-    var style = document.createElement('style');
-    style.innerHTML = `
-    /* Clickable phone numbers and usernames, admin's text for readonly groups, clickable notification message numbers */
-    div._26iqs.UxSU9, span[role="button"][class="_17SvR"], span._1adfa.eHxwV._3-8er {
-        pointer-events: none; 
-        cursor: default;
+class WhatsAppWebChatRestrictor {
+    constructor(visible_chat_names) {
+        console.log('Initializing the system...');
+        this.isPageReady = false;
+        this.element_selectors = {
+            app: "#app",
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div/div[1]/div/div/div[1]
+            chat: "._3m_Xw",
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div/div[1]/div/div/div[1]/div/div/div[2]/div[1]/div[1]/span
+            chat_name: ".ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr.i0jNr",
+            chat_pane: '#pane-side',
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div
+            real_click: "._2nY6U",
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div/div
+            chat_mini_image: "._3GlyB",
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]/div[1]/div/div/div[1]/div/div
+            chat_height_dominant: "._2nY6U",
+            //    /html/body/div/div[1]/div/div/div/div
+            multiple_pop: '._3J6wB',
+            //    /html/body/div/div[1]/div/div/div/div/div/div[2]/div[1]
+            multiple_pop_button_1: "._20C5O._1zOyO",
+            //    /html/body/div[1]/div/div/div/div/div/div/div[2]/div[2]
+            multiple_pop_button_2: "._20C5O._2Zdgs",
+            /* header, chat search bar, */
+            //    /html/body/div/div[1]/div[1]/div[3]/div/header
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div[1]
+            will_be_removed: ['header', 'div.uwk68']
+        };
+        this.pure_element_classes = {
+            // scroll bar class for paneside, should be removed
+            //    /html/body/div/div[1]/div[1]/div[3]/div/div[2]
+            pane_side_remove: '_20c87'
+        };
+        this.css = `/* Clickable phone numbers and usernames, admin's text for readonly groups, clickable notification message numbers */
+        div._26iqs.UxSU9, span[role="button"][class="_17SvR"], span._1adfa.eHxwV._3-8er {
+            pointer-events: none; 
+            cursor: default;
+        }
+        /* Forward button, Chat dropdown button, Group info -> Search My Contacts, sended images dropdown button, sended messages dropdown button*/
+        div._3nHC-, div._3dGJA._3qSKL, div[class="-ZdaK"][role="button"], div._39Lm1._36H8K._3_UDv, div._39Lm1._3qSKL{
+            visibility: hidden;
+        }
+        /* pane-side overflow-y:auto-> limit the chats by window resolution, must be visible*/ 
+        #pane-side{
+            overflow-y: visible;
+        }`;
+        this.visible_chat_names = visible_chat_names;
+        this.addCSS();
+        this.createChatObserver();
     }
-
-    /* Forward button, Chat dropdown button, Group info -> Search My Contacts, sended images dropdown button, sended messages dropdown button*/
-    div._3nHC-, div._3dGJA._3qSKL, div[class="-ZdaK"][role="button"], div._39Lm1._36H8K._3_UDv, div._39Lm1._3qSKL{
-        visibility: hidden;
+    addCSS() {
+        console.log('Adding the CSS...');
+        let style = document.createElement('style');
+        style.innerHTML = this.css;
+        document.head.appendChild(style);
     }
-
-    /* pane-side overflow-y:auto-> limit the chats by window resolution, must be visible*/ 
-    #pane-side{
-        overflow-y: visible;
+    createChatObserver() {
+        console.log('Creating the chat observer...');
+        let wwcr = this;
+        let observer = new MutationObserver(function () {
+            let chat_pane = document.querySelector(wwcr.element_selectors.chat_pane);
+            if (chat_pane) {
+                console.log('Chat section is detected!');
+                this.disconnect();
+                console.log('Chat observer is disconnected!')
+                wwcr.run();
+            }
+        });
+        let app = document.querySelector(this.element_selectors.app);
+        observer.observe(app, this.getObserverConfig());
     }
-    `;
-    document.head.appendChild(style);
-}
-
-function chatObserver() {
-    console.log('Chat observer is creating...');
-
-    var observer = new MutationObserver(function (mutations) {
-        var check = document.getElementById(chat_pane_id);
-        if (check) {
-            console.log('Chat section is detected!');
-
-            observer.disconnect();
-
-            console.log('Chat observer disconnected!')
-
-            start();
-        }
-    });
-    let app = document.getElementById('app');
-    observer.observe(app, observeConfigs());
-}
-
-function start() {
-    hideUnwantedChats();
-    if (!isPageReady) oneTimeCleanup();
-    changeObserver();
-}
-
-function hideUnwantedChats() {
-    console.log('Unwanted chats are hiding...');
-
-    chats = document.getElementsByClassName(chat_class);
-    // console.log(chats)
-    let heights = calculateHeights();
-    // console.log(heights)
-    let y_value = -heights.chat;
-    for (let chat of chats) {
-        // console.log(chat)
-        let chat_name_element = chat.getElementsByClassName(chat_name_class)[0];
-        // console.log(chat_name_element)
-        let chat_name = ''
-        if (chat_name_element) {
-            chat_name = trimSpaces(chat_name_element.innerText);
-        } else {
-            continue
-        }
-        // console.log(chat_name)
-        if (final_chats.includes(chat_name)) {
-            y_value += heights.chat;
-            chat_mini_image = chat.getElementsByClassName(chat_mini_image_class)[0];
-            // // console.log(chat_mini_image)
-            chat_mini_image.style.height = heights.image.toString() + "px";
-            chat_mini_image.style.width = heights.image.toString() + "px";
-            chat.style.height = heights.chat.toString() + "px";
-            chat.getElementsByClassName(chat_height_dominant_class)[0].style.height = heights.chat.toString() + "px";
-            chat.hidden = false;
-            // console.log(chat_name, 'görünür')
-        } else {
-            chat.hidden = true;
-            // console.log(chat_name, 'gizli')
-        }
-    };
-    // console.log(y_value)
-    for (let chat of chats) {
-        if (!chat.hidden) {
-            // console.log(y_value)
-            transform_value = "translateY(" + y_value.toString() + "px)";
-            chat.style.transform = transform_value;
-            y_value -= heights.chat;
-        }
-    };
-}
-
-function oneTimeCleanup() {
-    removeRiskyModules();
-
-    for (let chat of chats) {
-        if (!chat.hidden) {
-            var clickable_element = chat.getElementsByClassName(real_click_class)[0];
-            realLikeClick(clickable_element);
-            break;
-        }
+    run() {
+        this.hideUnwantedChats();
+        if (!this.isPageReady) this.oneTimeCleanup();
+        this.createChangeObserver();
     }
-}
-
-function changeObserver() {
-    console.log('Change observer is creating...');
-
-    let observer = new MutationObserver(mutations => {
-        console.log('Some changes...');
-
-        var multipleUsageInterrupt = document.getElementsByClassName(multiple_pop_class)[0]
-        // console.log(multipleUsageInterrupt)
-        if (multipleUsageInterrupt) {
-            console.log('In Use section is detected!');
-
-            document.getElementsByClassName(multiple_pop_button_1_class)[0].addEventListener("click", init);
-            document.getElementsByClassName(multiple_pop_button_2_class)[0].addEventListener("click", init);
-            observer.disconnect();
-
-            console.log('Change observer disconnected!');
-
-            return;
-        }
-
-
-        for (let mutation of mutations) {
-
-            if (mutation.target.matches(`div[class*="${chat_class}"]`) && mutation.type === 'attributes') {
-                console.log('Changes in the attributes of some chats...');
-
+    hideUnwantedChats() {
+        console.log('Hiding unwanted chats...');
+        let chats = document.querySelectorAll(this.element_selectors.chat);
+        let heights = this.getCalculatedHeights();
+        let y_value = -heights.chat;
+        for (let chat of chats) {
+            let chat_name_element = chat.querySelector(this.element_selectors.chat_name);
+            let chat_name = ''
+            if (chat_name_element) {
+                chat_name = trimSpaces(chat_name_element.innerText);
+            } else {
+                continue
+            }
+            if (this.visible_chat_names.includes(chat_name)) {
+                y_value += heights.chat;
+                let chat_mini_image = chat.querySelector(this.element_selectors.chat_mini_image);
+                chat_mini_image.style.height = heights.image.toString() + "px";
+                chat_mini_image.style.width = heights.image.toString() + "px";
+                chat.style.height = heights.chat.toString() + "px";
+                chat.querySelector(this.element_selectors.chat_height_dominant).style.height = heights.chat.toString() + "px";
+                chat.hidden = false;
+                // console.log(chat_name, 'görünür')
+            } else {
+                chat.hidden = true;
+                // console.log(chat_name, 'gizli')
+            }
+        };
+        for (let chat of chats) {
+            if (!chat.hidden) {
+                let transform_value = "translateY(" + y_value.toString() + "px)";
+                chat.style.transform = transform_value;
+                y_value -= heights.chat;
+            }
+        };
+    }
+    createChangeObserver() {
+        console.log('Creating the change observer...');
+        let wwcr = this;
+        let observer = new MutationObserver(mutations => {
+            console.log('Some changes...');
+            let multipleUsageInterrupt = document.querySelector(wwcr.element_selectors.multiple_pop)
+            if (multipleUsageInterrupt) {
+                console.log('In Use section is detected!');
+                let reconstruction = () => {
+                    new WhatsAppWebChatRestrictor(wwcr.visible_chat_names)
+                }
+                document.querySelector(wwcr.element_selectors.multiple_pop_button_1).addEventListener("click", reconstruction);
+                document.querySelector(wwcr.element_selectors.multiple_pop_button_2).addEventListener("click", reconstruction);
                 observer.disconnect();
-
-                console.log('Change observer disconnected!')
-
-                start();
+                console.log('Change observer disconnected!');
                 return;
             }
-
-            for (let node of mutation.addedNodes) {
-                // we track only elements, skip other nodes (e.g. text nodes)
-                if (!(node instanceof HTMLElement)) continue;
-
-                if (node.matches(`div[class*="${chat_class}"]`)) {
-                    console.log('New chats have arrived...');
-
+            for (let mutation of mutations) {
+                if (mutation.target.matches(`div[class*="${wwcr.element_selectors.chat}"]`) && mutation.type === 'attributes') {
+                    console.log('Changes in the attributes of some chats...');
                     observer.disconnect();
-
                     console.log('Change observer disconnected!')
-
-                    start();
+                    wwcr.run();
                     return;
                 }
+                for (let node of mutation.addedNodes) {
+                    // we track only elements, skip other nodes (e.g. text nodes)
+                    if (!(node instanceof HTMLElement)) continue;
+                    if (node.matches(`div[class*="${wwcr.element_selectors.chat}"]`)) {
+                        console.log('New chats have arrived...');
+                        observer.disconnect();
+                        console.log('Change observer disconnected!')
+                        wwcr.run();
+                        return;
+                    }
+                }
+            }
+        });
+        // let chat_pane = document.getElementById(chat_pane_id);
+        observer.observe(document, this.getObserverConfig());
+    }
+    oneTimeCleanup() {
+        let chats = document.querySelectorAll(this.element_selectors.chat);
+        this.removeRiskyModules();
+        for (let chat of chats) {
+            if (!chat.hidden) {
+                let clickable_element = chat.querySelector(this.element_selectors.real_click);
+                realLikeClick(clickable_element);
+                break;
             }
         }
-    });
-    // let chat_pane = document.getElementById(chat_pane_id);
-    observer.observe(document, observeConfigs());
-}
-
-function removeRiskyModules() {
-    console.log('Risky modules are deleting...');
-
-    pane_side = document.getElementById(chat_pane_id);
-    pane_side.classList.remove(pane_side_remove_class);
-
-    for (const selector of will_be_removed) {
-        var element = document.querySelector(selector);
-        if (element) {
-            element.parentNode.removeChild(element);
-        } else {
-            break;
-        }
+    }
+    removeRiskyModules() {
+        console.log('Deleting risky modules...');
+        let pane_side = document.querySelector(this.element_selectors.chat_pane);
+        pane_side.classList.remove(this.pure_element_classes.pane_side_remove);
+        for (let element of document.querySelectorAll(this.element_selectors.will_be_removed)) {
+            if (element) {
+                element.parentNode.removeChild(element);
+            } else {
+                break;
+            }
+        };
+        this.isPageReady = true;
     };
+    getCalculatedHeights() {
+        let default_chat_height = 72;
+        let default_img_height = 49;
+        let viewer_height = document.documentElement.clientHeight
+        if (this.visible_chat_names.length * default_chat_height > viewer_height) {
+            let new_chat_height = viewer_height / final_chats.length;
+            let ratio = new_chat_height / default_chat_height;
+            let new_img_height = ratio * default_img_height;
 
-    isPageReady = true;
-};
+            return {
+                image: new_img_height,
+                chat: new_chat_height
+            }
+        } else {
+            return {
+                image: default_img_height,
+                chat: default_chat_height
+            }
+        }
+    }
+    getObserverConfig() {
+        return {
+            attributes: true,
+            childList: true,
+            characterData: false,
+            subtree: true
+        };
+    };
+}
 
 function realLikeClick(element) {
     console.log('Real like click is simulating...');
-
     var simulateMouseEvent = function (element, eventName, coordX, coordY) {
         element.dispatchEvent(new MouseEvent(eventName, {
             view: window,
@@ -216,35 +216,16 @@ function realLikeClick(element) {
             button: 0
         }));
     };
-
     var box = element.getBoundingClientRect(),
         coordX = box.left + (box.right - box.left) / 2,
         coordY = box.top + (box.bottom - box.top) / 2;
-
     simulateMouseEvent(element, "mousedown", coordX, coordY);
     simulateMouseEvent(element, "mouseup", coordX, coordY);
     simulateMouseEvent(element, "click", coordX, coordY);
 };
 
 function trimSpaces(text) {
-    return text.split(/(\s+)/).filter(function (e) { return e.trim().length > 0; }).join(' ');
+    return text.split(/(\s+)/).filter(function (e) {
+        return e.trim().length > 0;
+    }).join(' ');
 }
-
-function calculateHeights() {
-    let default_chat_height = 72;
-    let default_img_height = 49;
-    let viewer_height = document.documentElement.clientHeight
-    if (final_chats.length * default_chat_height > viewer_height) {
-        let new_chat_height = viewer_height / final_chats.length;
-        let ratio = new_chat_height / default_chat_height;
-        let new_img_height = ratio * default_img_height;
-
-        return { image: new_img_height, chat: new_chat_height }
-    } else {
-        return { image: default_img_height, chat: default_chat_height }
-    }
-}
-
-function observeConfigs() {
-    return { attributes: true, childList: true, characterData: false, subtree: true };
-};
